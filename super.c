70cde18d00ace758c21dd85d5647a1e3c3cddb4b
José Júnior <86629800+jjosejr@users.noreply.github.com>
2025-05-21 11:17:29 -0300

    atualizei o super

--- super.c
+++ super.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+int main() {
+    int carta1, carta2;
+
+    
+    printf("Digite o valor da primeira carta: ");
+    scanf("%d", &carta1);
+
+    printf("Digite o valor da segunda carta: ");
+    scanf("%d", &carta2);
+ 
+    if (carta1 > carta2) {
+        printf("Carta 1 venceu com ataque %d contra %d!\n", carta1, carta2);
+    } else if (carta2 > carta1) {
+        printf("Carta 2 venceu com ataque %d contra %d!\n", carta2, carta1);
+    } else {
+        printf("Empate! Ambas as cartas têm ataque %d.\n", carta1);
+    }
+
+    return 0;
+}
